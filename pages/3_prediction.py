64ce2d8b9ecf510fc7cc22784bbe3ebafd3529c1
Lorcan Rae <lorcanrae@outlook.com>
2022-12-07 18:30:19 +0000

    3_predict working with dual feed model

--- pages/3_prediction.py
+++ pages/3_prediction.py
@@ -6,28 +6,29 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from wordcloud import STOPWORDS
 from PIL import Image
-import random
 
-from scam_spotter_web.main import predict_all
-from scam_spotter_web.predict import load_model
+from scam_spotter_web.main import predict_dual, predict_nlp
+from scam_spotter_web.load_models import load_model
 
-#Live demo of model
-#Takes URL and spits out image (and wordcloud)
-#And gives us a probability that website is scam
+# Live demo of model
+# Takes URL and spits out image (and wordcloud)
+# And gives us a probability that website is scam
 
-MODEL_PATH = 'models/model2'
-MODEL = load_model(MODEL_PATH)
+
+DUAL_MODEL_PATH = 'models/model2'
+NLP_MODEL_PATH = 'models/nlp_model.h5'
+MODEL = load_model(DUAL_MODEL_PATH)
 
 st.markdown('## Is that website a scam?')
 
 website = st.text_input('Enter URL here in https:// format')
 
-predict = st.button('Click to see if that website is a scam!')
+make_predict = st.button('Click to see if that website is a scam!')
 
-if predict:
-    prediction = predict_all(MODEL, website)
+if make_predict:
+    prediction = predict_dual(MODEL, website)[0][0] * 100
 
     if prediction:
         out_text = prediction
 
-        st.success(f'Probability of this site being a scam is {out_text}')
+        st.success(f'Probability of this site being a scam is {out_text: .3f} %!')
